1109c35127470ebd9f6324121fab98b701da4617
NazrinGulmammadova <6jauppt@code.edu.az>
2022-11-02 00:31:37 +0400

    created tasks

---
+++ task2.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = { 3, 6, 9 };
+int largestNumber = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > largestNumber)
+    {
+        largestNumber = arr[i];
+    }
+}
+Console.WriteLine(largestNumber);
+
